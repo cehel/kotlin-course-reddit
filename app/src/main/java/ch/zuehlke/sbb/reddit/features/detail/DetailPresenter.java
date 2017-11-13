d85b8418ec465c06b96fa97d7d1e51d26e2da174
chsc <christian.gauch@zuehlke.com>
2017-11-13 14:09:46 +0100

    - Added example method to flatten the reddit response - Removed getter and setter from the retrofit model classes

--- app/src/main/java/ch/zuehlke/sbb/reddit/features/detail/DetailPresenter.java
+++ app/src/main/java/ch/zuehlke/sbb/reddit/features/detail/DetailPresenter.java
@@ -3,6 +3,7 @@ package ch.zuehlke.sbb.reddit.features.detail;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.common.base.Strings;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -10,6 +11,7 @@ import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import ch.zuehlke.sbb.reddit.data.source.RedditRepository;
@@ -71,7 +73,7 @@ public class DetailPresenter implements DetailContract.Presenter {
 
                 try {
                     final List<RedditPostElement> redditPostElements = gson.fromJson(response.body().string(), type);
-                    List<RedditPost> parsedElements = flattenResponse(redditPostElements);
+                    List<RedditPost> parsedElements = flattenRetrofitResponse(redditPostElements);
                     if(mDetailView.isActive()){
                         mDetailView.showRedditPosts(parsedElements);
                     }
@@ -89,28 +91,40 @@ public class DetailPresenter implements DetailContract.Presenter {
         });
     }
 
-
-    private List<RedditPost> flattenResponse(List<RedditPostElement> elements){
-        List<RedditPost> flattenList = new ArrayList<>();
-        for (RedditPostElement element : elements) {
-            if(element instanceof RedditPostElement.DataRedditPostElement){
-                flatten(flattenList,element);
+    private List<RedditPost> flattenRetrofitResponse(List<RedditPostElement> response){
+        List<RedditPost> flatten = new ArrayList<>();
+        for (RedditPostElement redditPostElement : response) {
+            if(redditPostElement instanceof RedditPostElement.DataRedditPostElement){
+                RedditPostElement.DataRedditPostElement dataElement = (RedditPostElement.DataRedditPostElement) redditPostElement;
+                if(dataElement.data != null){
+                    if(!Strings.isNullOrEmpty(dataElement.data.body_html)){
+                        flatten.add(dataElement.data);
+                    }else{
+                        flatten.addAll(recursivlyParseResponse(dataElement));
+                    }
+                }
             }
         }
-        return flattenList;
+        return flatten;
     }
 
-    private List<RedditPost>flatten(List<RedditPost> currentList,RedditPostElement element){
-        if(((RedditPostElement.DataRedditPostElement) element).data != null){
-            if(((RedditPostElement.DataRedditPostElement) element).data.replies != null && ((RedditPostElement.DataRedditPostElement) element).data.replies instanceof RedditPostElement.DataRedditPostElement){
-                RedditPostElement.DataRedditPostElement childElement = (RedditPostElement.DataRedditPostElement) ((RedditPostElement.DataRedditPostElement) element).data.replies;
-                currentList.addAll(flatten(currentList,childElement));
+    private List<RedditPost> recursivlyParseResponse(RedditPostElement.DataRedditPostElement dataRedditPostElement){
+        List<RedditPost> posts = new ArrayList<>();
+        for (RedditPostElement child : dataRedditPostElement.data.children) {
+            if(child instanceof RedditPostElement.DataRedditPostElement){
+                RedditPostElement.DataRedditPostElement childData = (RedditPostElement.DataRedditPostElement) child;
+                if(childData.data != null){
+                    posts.add(childData.data);
+                    if(childData.data.replies != null && childData.data.replies instanceof RedditPostElement.DataRedditPostElement){
+                        posts.addAll(recursivlyParseResponse((RedditPostElement.DataRedditPostElement) childData.data.replies));
+                    }
+                }
             }
-        }
-        currentList.add(((RedditPostElement.DataRedditPostElement) element).data);
 
-        return currentList;
+        }
+        return posts;
     }
+
 
 
 }
