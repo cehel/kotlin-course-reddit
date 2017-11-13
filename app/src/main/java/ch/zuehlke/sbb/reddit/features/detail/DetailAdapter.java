1b721ce93a0ec992a2b53f78b12fc29163a3abbe
chsc <christian.gauch@zuehlke.com>
2017-11-13 15:33:13 +0100

    - Added detail screen

---
+++ app/src/main/java/ch/zuehlke/sbb/reddit/features/detail/DetailAdapter.java
@@ -0,0 +1,83 @@
+package ch.zuehlke.sbb.reddit.features.detail;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.zuehlke.sbb.reddit.R;
+import ch.zuehlke.sbb.reddit.data.source.remote.model.posts.RedditPost;
+import ch.zuehlke.sbb.reddit.util.DateUtils;
+
+/**
+ * Created by chsc on 13.11.17.
+ */
+
+public class DetailAdapter extends RecyclerView.Adapter<DetailAdapter.DetailViewHolder>{
+
+
+    private List<RedditPost> mItems = new ArrayList<>();
+    private Context mContext;
+    public DetailAdapter(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public DetailViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_detail, parent, false);
+        return new DetailViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DetailViewHolder holder, int position) {
+        RedditPost post = mItems.get(position);
+
+        holder.mUsername.setText(post.author);
+        holder.mText.setText(post.body);
+        holder.mCreated.setText(DateUtils.friendlyTime(post.created_utc));
+        setDepthPadding(holder.itemView,post.depth);
+    }
+
+    public void clearAndAddPosts(List<RedditPost> newsData){
+        int previousItemSize = mItems.size();
+        mItems.clear();
+        notifyItemRangeRemoved(0,previousItemSize);
+        mItems.addAll(newsData);
+        notifyDataSetChanged();
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    private void setDepthPadding(View view, int depth){
+        float normalPadding = 5f;
+        float leftPadding= normalPadding + (depth * 10f);
+
+        int normalDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, normalPadding, mContext.getResources().getDisplayMetrics());
+        int leftDp = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, leftPadding, mContext.getResources().getDisplayMetrics());
+        view.setPadding(leftDp, normalDp, normalDp, normalDp);
+    }
+
+    static class DetailViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView mUsername;
+        private TextView mCreated;
+        private TextView mText;
+
+        public DetailViewHolder(View itemView) {
+            super(itemView);
+            mUsername = itemView.findViewById(R.id.username);
+            mCreated = itemView.findViewById(R.id.created);
+            mText = itemView.findViewById(R.id.text);
+        }
+    }
+}
--- app/src/main/java/ch/zuehlke/sbb/reddit/features/detail/DetailFragment.java
+++ app/src/main/java/ch/zuehlke/sbb/reddit/features/detail/DetailFragment.java
@@ -3,16 +3,24 @@ package ch.zuehlke.sbb.reddit.features.detail;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import java.util.List;
 
 import ch.zuehlke.sbb.reddit.R;
 import ch.zuehlke.sbb.reddit.data.source.remote.model.posts.RedditPost;
+import ch.zuehlke.sbb.reddit.features.overview.InfiniteScrollListener;
+import ch.zuehlke.sbb.reddit.features.overview.ScrollChildSwipeRefreshLayout;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -25,6 +33,9 @@ public class DetailFragment extends Fragment implements DetailContract.View {
     private static final String TAG = "DetailFragment";
 
     private DetailContract.Presenter mPresenter;
+    private DetailAdapter mAdapter;
+    private RecyclerView mPostView;
+    private View mNoPostView;
 
 
     public DetailFragment() {
@@ -36,10 +47,42 @@ public class DetailFragment extends Fragment implements DetailContract.View {
     }
 
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAdapter = new DetailAdapter(getContext());
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        mNoPostView = root.findViewById(R.id.noRedditPostVIew);
+        mPostView = root.findViewById(R.id.redditPostView);
+        mPostView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mPostView.setAdapter(mAdapter);
+
+        // Set up progress indicator
+        final ScrollChildSwipeRefreshLayout swipeRefreshLayout =
+                (ScrollChildSwipeRefreshLayout) root.findViewById(R.id.refreshLayout);
+        swipeRefreshLayout.setColorSchemeColors(
+                ContextCompat.getColor(getActivity(), R.color.colorPrimary),
+                ContextCompat.getColor(getActivity(), R.color.colorAccent),
+                ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark)
+        );
+
+
+        swipeRefreshLayout.setScrollUpChild(mPostView);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+               mPresenter.loadRedditPosts();
+            }
+        });
+
+
+        mPostView.setHasFixedSize(true);
         return root;
     }
 
@@ -63,5 +106,26 @@ public class DetailFragment extends Fragment implements DetailContract.View {
     @Override
     public void showRedditPosts(List<RedditPost> posts) {
         Log.i(TAG,"Got "+posts.size() + " posts");
+        mAdapter.clearAndAddPosts(posts);
+    }
+
+    @Override
+    public void showRedditNewsLoadingError() {
+        Snackbar.make(getView(),R.string.overview_screen_error_loading_reddit_posts,Snackbar.LENGTH_LONG);
+    }
+
+    @Override
+    public void setLoadingIndicator(final boolean isActive) {
+        if (getView() == null) {
+            return;
+        }
+        final SwipeRefreshLayout srl = getView().findViewById(R.id.refreshLayout);
+        // Make sure setRefreshing() is called after the layout is done with everything else.
+        srl.post(new Runnable() {
+            @Override
+            public void run() {
+                srl.setRefreshing(isActive);
+            }
+        });
     }
 }
