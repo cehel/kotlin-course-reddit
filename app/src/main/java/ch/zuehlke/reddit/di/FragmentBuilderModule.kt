6ddc19bc689e49ed9a1c451df1a7ab1f5d6d67c9
cehel <Celine.Heldner@zuehlke.com>
2018-03-15 07:52:41 +0100

    DetailFragment and Adapter changes implemented

--- app/src/main/java/ch/zuehlke/reddit/di/FragmentBuilderModule.kt
+++ app/src/main/java/ch/zuehlke/reddit/di/FragmentBuilderModule.kt
@@ -1,6 +1,9 @@
 package ch.zuehlke.reddit.di
 
 import ch.zuehlke.reddit.di.scope.FragmentScope
+import ch.zuehlke.reddit.features.login.LoginFragment
+import ch.zuehlke.reddit.features.news.detail.DetailFragment
+import ch.zuehlke.reddit.features.news.detail.DetailFragmentModule
 import ch.zuehlke.reddit.features.news.overview.OverviewFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
@@ -11,8 +14,13 @@ import dagger.android.ContributesAndroidInjector
 @Module
 public abstract class FragmentBuilderModule{
 
+    @ContributesAndroidInjector
+    @FragmentScope
+    abstract fun contributeLoginFragment(): LoginFragment
 
-
+    @ContributesAndroidInjector(modules = arrayOf(DetailFragmentModule::class))
+    @FragmentScope
+    abstract fun contributeDetailFragment(): DetailFragment
 
     @ContributesAndroidInjector
     @FragmentScope
